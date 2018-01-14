c7c066bb426f29aec9f8bc4bc7b2f889b4cdd7bc
Lisa Lee <snickers495@gmail.com>
2018-01-14 19:19:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,4 +25,9 @@ class Trip {
     store.trips.push(this);
 
   }
+  passenger(){
+    return store.passengers.find(function(passenger){
+      return passenger.id === this.passengerId;
+    })
+  }
 }
