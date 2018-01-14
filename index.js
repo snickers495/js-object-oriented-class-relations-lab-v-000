90fbd660023a8c685c511f1afdca16acf1f16691
Lisa Lee <snickers495@gmail.com>
2018-01-14 19:16:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,7 +22,8 @@ class Trip {
     this.driver = driver;
     this.passenger = passenger;
     this.id = ++tripId;
-    this.driverId = driver.id
+    this.driverId = driver.id;
+    this.passengerId = passenger.id
     store.trips.push(this);
 
   }
